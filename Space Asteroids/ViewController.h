ab8c6223d409a7e416bb5929527535262aeaf2e9
cvanderschuere <christopher.vanderschuere@gmail.com>
2012-11-29 11:49:59 -0800

    Initial Commit

---
+++ Space Asteroids/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Space Asteroids
+//
+//  Created by kuwaharg on 11/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Space Asteroids/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Space Asteroids
+//
+//  Created by kuwaharg on 11/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
