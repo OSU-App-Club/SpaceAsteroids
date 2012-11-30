ae04d1f5cdf6a4c43f3044fb642f8643b068958c
cvanderschuere <cdvconcepts@gmail.com>
2012-11-30 14:48:17 -0800

    Added CMMotionManager example

--- Space Asteroids/ViewController.m
+++ Space Asteroids/ViewController.m
@@ -8,13 +8,18 @@
 
 #import "ViewController.h"
 
-@implementation ViewController
+//Create a private interface for things we don't want others to see/use
+@interface ViewController ()
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
-}
+@property (nonatomic, strong) CMMotionManager * motionManager; //Handles all the sensor data from a device
+
+@end
+
+
+
+@implementation ViewController
+@synthesize motionManager = _motionManager;
+
 
 #pragma mark - View lifecycle
 
@@ -22,6 +27,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Create CMMotionManager only once on startup
+    self.motionManager = [[CMMotionManager alloc] init];
+    NSLog(@"Screen Width: %f Height: %f",self.view.bounds.size.width,self.view.bounds.size.height);
+
 }
 
 - (void)viewDidUnload
@@ -29,11 +39,28 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    
+    //Start motion updates
+    self.motionManager.deviceMotionUpdateInterval = .5f;
+    [self.motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:
+        ^(CMDeviceMotion * motion, NSError *error){
+            //This is a block...think of it as an inline function
+            
+            //CMDevice Motion contains information about the "attitude" of the device, or angles relative to the X,Y, and Z axes
+            CMAttitude *currentAttitude = motion.attitude;
+            
+            //It is always helpful to print out the data to see what you are working with
+            NSLog(@"Pitch: %f Roll %f Yaw: %f",currentAttitude.pitch,currentAttitude.roll,currentAttitude.yaw);
+    
+        }
+     ];
+    
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -44,6 +71,9 @@
 - (void)viewWillDisappear:(BOOL)animated
 {
 	[super viewWillDisappear:animated];
+    
+    //Stop updates when they are not needed anymore
+    [self.motionManager stopDeviceMotionUpdates];
 }
 
 - (void)viewDidDisappear:(BOOL)animated
@@ -51,14 +81,5 @@
 	[super viewDidDisappear:animated];
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
-}
 
 @end
